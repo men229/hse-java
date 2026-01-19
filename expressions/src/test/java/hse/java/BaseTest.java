8e04cdabf4e9fd683b49b07a0bb8fb244c8075b2
Mike <levinmk23@gmail.com>
2026-01-19 18:12:49 +0300

    Test PR

--- expressions/src/test/java/hse/java/BaseTest.java
+++ expressions/src/test/java/hse/java/BaseTest.java
@@ -1,9 +1,14 @@
 package hse.java;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class BaseTest {
 
     @Test
-    void test() {}
+    void test() {
+        int actually = 1;
+        Assertions.assertEquals(1, actually);
+    }
 }
