a899d74087c8479c249856a2107e71b673bf7016
root <root@LAPTOP-SEMDMQRP.localdomain>
2026-02-11 15:46:41 +0300

    html lecture3

--- src/main/java/hse/java/lectures/lecture3/tasks/html/HtmlDocument.java
+++ src/main/java/hse/java/lectures/lecture3/tasks/html/HtmlDocument.java
@@ -27,6 +27,83 @@ public class HtmlDocument {
         }
     }
 
-    private void validate(String content){}
+    private void validate(String content) {
+        Deque<String> deque_part = new ArrayDeque<>();
+        int[] used = new int[3];
+        Set<String> set = Set.of("html", "head", "body", "div", "p");
+        boolean open_head = false;
+        boolean open_body = false;
 
+        for (int i = 0; i < content.length(); i++) {
+            int flag = 0;
+            StringBuilder str = new StringBuilder();
+            char c = content.charAt(i);
+            if (c == '<') {
+                i++;
+                if (content.charAt(i) == '/') {
+                    i += 1;
+                    flag = 1;
+                }
+                c = content.charAt(i);
+                while (c != '>' && c != ' ') {
+                    str.append(c);
+                    i++;
+                    c = content.charAt(i);
+                }
+                if (!set.contains(str.toString().toLowerCase())) {
+                    throw new UnsupportedTagException("Invalid command");
+                }
+            }
+            if (str.toString().isEmpty() && !set.contains(str.toString())) {
+                continue;
+            }
+            if (str.toString().equalsIgnoreCase("html")) {
+                if (used[0] >= 2) {
+                    throw new InvalidStructureException("Invalid struct");
+                }
+                used[0] += 1;
+            } else if (str.toString().equalsIgnoreCase("head")) {
+                open_head = true;
+                if(used[0] == 0 || used[2] > 0){
+                    throw new InvalidStructureException("Invalid struct");
+                }
+                if (used[1] >= 2) {
+                    throw new InvalidStructureException("Invalid struct");
+                }
+                used[1] += 1;
+            }else if (str.toString().equalsIgnoreCase("body")) {
+                open_body = true;
+                if (used[2] >= 2) {
+                    throw new InvalidStructureException("Invalid struct");
+                }
+                used[2] += 1;
+            }
+
+            if (flag == 0) {
+
+                if (c == ' ') {
+                    while (c != '>') {
+                        i++;
+                        c = content.charAt(i);
+                    }
+                }
+                deque_part.offerFirst(str.toString().toLowerCase());
+            } else {
+                if (deque_part.isEmpty()) {
+                    throw new UnexpectedClosingTagException("Unexpected tag");
+                }
+                if (deque_part.getFirst().contentEquals(str.toString().toLowerCase())) {
+                    deque_part.removeFirst();
+                } else {
+                    throw new MismatchedClosingTagException("Mismatched tag");
+                }
+            }
+            if(!open_head && !open_body && !str.toString().equalsIgnoreCase("html")){
+                throw new InvalidStructureException("Invalid struct");
+            }
+        }
+        if (!deque_part.isEmpty()) {
+            throw new UnclosedTagException("Unclosed tag");
+        }
+    }
 }
